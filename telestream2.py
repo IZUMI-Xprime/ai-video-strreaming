c6e66ae512b53643fedaf315ec0b2955a5e01bd3
IZUMI-Xprime <darnishcnpm@gmail.com>
2024-12-21 23:17:04 +0530

    Update telestream2.py

--- telestream2.py
+++ telestream2.py
@@ -213,5 +213,6 @@ def download_video(filename):
 
 
 if __name__ == "__main__":
-    reconnect_stream()
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.getenv("PORT", 5000))  # Default to port 5000 if PORT is not set
+    app.run(host="0.0.0.0", port=port)
+
